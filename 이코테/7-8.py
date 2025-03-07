cb981023f0728d682ef386fea290619f70377ffa
kwarkmc <kwarkmc99@gmail.com>
2025-03-07 10:29:36 +0900

    Add Daily PS 2025-03-07

---
+++ 이코테/7-8.py
@@ -0,0 +1,20 @@
+n, m = map(int, input().split())
+
+array = list(map(int, input().split()))
+
+start = 0
+end = max(array)
+
+while start <= end:
+    mid = (start + end) // 2
+    total = 0
+    for i in array:
+        if i > mid:
+            total += i - mid
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+
+print(result)
